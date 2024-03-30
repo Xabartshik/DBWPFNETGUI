aa9bb092e7f62b5e3fca132db4430056d778a326
Xabartshik <danoshlakov12@gmail.com>
2024-03-31 00:22:45 +0900

    Добавьте файлы проекта.

---
+++ DBWPFNETGUI/GreatBookOfGrudgesRecord.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Windows;
+using System.Security.Policy;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Xml;
+namespace DBWPFNETGUI
+{
+
+    //https://waha.fandom.com/ru/wiki/%D0%9A%D0%BD%D0%B8%D0%B3%D0%B8_%D0%9E%D0%B1%D0%B8%D0%B4
+    // Класс, представляющий модель данных для одной записи в Великой Книге Обид
+    public class GreatBookOfGrudgesRecord
+    {
+        // Приватное поле для хранения номера обиды
+        private uint _grudgeNumber;
+
+        // Приватное поле для хранения описания обиды
+        private string _grudge;
+
+        // Приватное поле для хранения даты нанесения обиды
+        private string _dateOfWrongdoing;
+
+        // Приватное поле для хранения имени обидчика
+        private string _foolName;
+
+        // Приватное поле для хранения статуса искупления обиды
+        private string _redemptionStatus;
+
+        // Приватное поле для хранения имени свидетеля обиды
+        private string _witness;
+
+        // Приватное поле для хранения доказательств обиды
+        private string _evidence;
+
+        // Приватное поле для хранения уровня обиды
+        private string _grudgeLevel;
+
+        ////Новое событие
+        //public event PropertyChangedEventHandler PropertyChanged;
+
+        //public void OnPropertyChanged(string propertyName)
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        //}
+        // Конструктор по умолчанию
+        public GreatBookOfGrudgesRecord()
+        {
+            _grudgeNumber = 0;
+            _grudge = "";
+            _dateOfWrongdoing = "";
+            _foolName = "";
+            _redemptionStatus = "";
+            _witness = "";
+            _evidence = "";
+            _grudgeLevel = "";
+        }
+
+        // Конструктор с параметрами
+        public GreatBookOfGrudgesRecord(uint grudgeNumber, string grudge, string dateOfWrongdoing, string foolName, string redemptionStatus, string witness, string evidence, string grudgeLevel)
+        {
+            _grudgeNumber = grudgeNumber;
+            _grudge = grudge;
+            _dateOfWrongdoing = dateOfWrongdoing;
+            _foolName = foolName;
+            _redemptionStatus = redemptionStatus;
+            _witness = witness;
+            _evidence = evidence;
+            _grudgeLevel = grudgeLevel;
+        }
+
+        // Свойство для получения или установки номера обиды
+        [Key]
+        public uint GrudgeNumber
+        {
+            get { return _grudgeNumber; }
+            set
+            {
+                _grudgeNumber = value;
+                //OnPropertyChanged("GrudgeNumber");
+            }
+        }
+        // Свойство для получения или установки описания обиды
+        public string Grudge
+        {
+            get { return _grudge; }
+            set
+            {
+                _grudge = value;
+                //OnPropertyChanged("Grudge");
+            }
+        }
+
+        // Свойство для получения или установки даты нанесения обиды
+        public string DateOfWrongdoing
+        {
+            get { return _dateOfWrongdoing; }
+            set
+            {
+                _dateOfWrongdoing = value;
+                //OnPropertyChanged("DateOfWrongdoing");
+            }
+        }
+
+        // Свойство для получения или установки имени обидчика
+        public string FoolName
+        {
+            get { return _foolName; }
+            set
+            {
+                _foolName = value;
+                //OnPropertyChanged("FoolName");
+            }
+        }
+
+        // Свойство для получения или установки статуса искупления обиды
+        public string RedemptionStatus
+        {
+            get { return _redemptionStatus; }
+            set
+            {
+                _redemptionStatus = value;
+                //OnPropertyChanged("RedemptionStatus");
+            }
+        }
+
+        // Свойство для получения или установки имени свидетеля обиды
+        public string Witness
+        {
+            get { return _witness; }
+            set
+            {
+                _witness = value;
+                //OnPropertyChanged("Witness");
+            }
+        }
+
+        // Свойство для получения или установки доказательств обиды
+        public string Evidence
+        {
+            get { return _evidence; }
+            set
+            {
+                _evidence = value;
+                //OnPropertyChanged("Evidence");
+            }
+        }
+
+        // Свойство для получения или установки уровня обиды
+        public string GrudgeLevel
+        {
+            get { return _grudgeLevel; }
+            set
+            {
+                _grudgeLevel = value;
+                //OnPropertyChanged("GrudgeLevel");
+            }
+        }
+
+
+    }
+    //Класс, представляющий собой книгу обид. Он хранит в себе записи GreatBookOfGrudgesRecord в ObservableCollection 
+    public class GreatBookOfGrudges
+    {
+        private string _databasePath;
+        //Информация об обидах
+        public ObservableCollection<GreatBookOfGrudgesRecord> Records { get; set; }
+        //Конструктор. Проверяет, существует ли файл. Если он существует, то загружает из файла, если нет, то не загружает
+        public GreatBookOfGrudges(string databasePath)
+        {
+            _databasePath = databasePath;
+            Records = new ObservableCollection<GreatBookOfGrudgesRecord>();
+            bool fileExists = File.Exists(_databasePath);
+        }
+
+        public void UpdateRecord(GreatBookOfGrudgesRecord record)
+        {
+            // Находим индекс записи в коллекции
+            int index = Records.IndexOf(record);
+
+            // Если запись найдена, обновляем ее
+            if (index >= 0)
+            {
+                Records[index] = record;
+                //OnPropertyChanged("Records"); // Уведомляем об изменении коллекции Records
+            }
+        }
+
+
+    }
+
+    public class BookContext : DbContext
+    {
+        public DbSet<GreatBookOfGrudgesRecord> Records { get; set; } = null!;
+        public string _path;
+        public BookContext(string databasePath)
+        {
+            _path = databasePath;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source={_path}");
+        }
+    }
+    public class SQLiteHelper
+    {
+        static public string _databasePath { get; set; }
+
+        public SQLiteHelper(string databasePath)
+        {
+            _databasePath = databasePath;
+        }
+
+        public static async void SaveObservableCollection(ObservableCollection<GreatBookOfGrudgesRecord> records)
+        {
+            //Создание контекста
+            var dbContext = new BookContext(_databasePath);
+            //Заставляем точно создать базу данных, если ее не было
+            dbContext.Database.EnsureCreated();
+            //Сохранение значений
+            using (var context = new BookContext(_databasePath))
+            {
+                //Очистка базы данных. Закомментировать, если не нужно. Иначе Номер Обиды будет увеличиваться с каждой новой записью
+                //File.Delete(_databasePath);
+                context.Records.RemoveRange(context.Records);
+                await context.SaveChangesAsync();
+                context.Records.AddRange(records);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public static ObservableCollection<GreatBookOfGrudgesRecord> LoadObservableCollection()
+        {
+            using (var context = new BookContext(_databasePath))
+            {
+                var records = context.Records.ToList();
+                
+                return new ObservableCollection<GreatBookOfGrudgesRecord>(records);
+            }
+        }
+
+    }
+}
---
+++ DBWPFNETGUI/MainWindow.xaml.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DBWPFNETGUI;
+/* Сделал -- Ошлаков Данил, ИВТ-22
+ * Требования
+Разделение представления и модели (данных и методов работы с ними).
+
+Одна таблица с 4+ полями === КНИГА ОБИД.
+
+Собственный формат БД или SQL (QSLite или серверная БД)
+
+Добавление, проверка, изменение, удаление данных
+
+Поиск, сортировка (как минимум по одному полю).
+
+Документация (в коде) описывающая формат данных в файле (если используется).
+
+Требования к GUI:
+
+вывод данных в таблицу (но не хранение в этом компоненте!)
+меню приложения (menu) DONE
+панель инструментов (toolbar) DONE
+шрифт и цветовая палитра отличные от задаваемых по умолчанию. DONE
+иконка приложения DONE
+Всплывающая подсказка или подсказка в строке состояния для элементов интерфейса. ToolTip="Всплывающая подсказка для кнопки" DONE
+◦ Информация о разработчике. 
+◦ Дополнительно (выполните минимум 3 пункта): 
+▪ горячие клавиши +++
+▪ цветовое кодирование данных в таблице +++ (если живой, то зеленый, если мертвый, то красный)
+▪------------------------------------ использование элементов интерфейса (флажок, числовое поле ввода и тт.п.) в таблице ??? (М.б. в настройках сделать, типа отображения) (Done, MenuItem IsCheckable)
+▪ хранение изображений в БД ---
+▪ использовать как минимум одно модальное окно +++ 
+▪ Автоматическое сохранение БД через заданные интервалы времени +++ 
+▪------------------------------------ краткая справка +++ (Типа готово?)
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+namespace DBWPFNETGUI
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private GreatBookOfGrudges greatBookOfGrudges = new GreatBookOfGrudges("blabla.db");
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Настройка
+            dataGrid.CanUserAddRows = false;
+            //Связывание источника и dataGrid
+            dataGrid.ItemsSource = greatBookOfGrudges.Records;
+            //Куда сохранить ДБ
+            //SQLiteHelper._databasePath = "D:\\SDK\\Projects\\GUI\\VS\\DBWPFNETGUI\\DBWPFNETGUI\\bin\\Debug\\net8.0-windows\\Grudges.db";
+            SQLiteHelper._databasePath = "Grudges.db";
+            dataGrid.IsReadOnly = false;
+        }
+
+        //Обработчик события нажатия на кнопку О разработчике. Выводит информацию о разработчике.
+        private void MIAbDeveloper_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Сделал: Ошлаков Данил, ИВТ-22", "О разработчике", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        //Обработчик события нажатия на кнопку О программе. Выводит информацию о программе.
+        private void MIAbProgram_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Небольшая База Данных 'Книга Обид'. В ней хранится \nинформация о том, кто, когда и как обидел хозяина этой книги. \nРазработана для дварфов.", "О программе", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+
+
+        //Процедура для выбора файла для сохранения
+        public static string GetSelectedFileName()
+        {
+            // Создаем экземпляр класса OpenFileDialog
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            // Устанавливаем начальный каталог
+            openFileDialog.InitialDirectory = "C:\\";
+
+            // Устанавливаем фильтр файлов
+            openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+
+            // Отображаем диалоговое окно
+            if (openFileDialog.ShowDialog() == true)
+            {
+                // Возвращаем выбранный файл
+                return openFileDialog.FileName;
+            }
+
+            // Если файл не выбран, возвращаем пустую строку
+            return "";
+        }
+        //Обработчик событий кнопки "Загрузить". Загружает файл базы данных, вызывая диалоговое окно
+        private void MIFLoad_Click(object sender, RoutedEventArgs e)
+        {
+            //Загрузка данных
+            greatBookOfGrudges.Records = SQLiteHelper.LoadObservableCollection();
+            //Поскольку операция загрузки возвращает новый объект, нужно заново укзать на источник данных
+            dataGrid.ItemsSource = greatBookOfGrudges.Records;
+            MessageBox.Show("Загружено");
+        }
+        //Обработчик кнопки Добавить. Добавляет пустую строку
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            greatBookOfGrudges.Records.Add(new GreatBookOfGrudgesRecord());
+        }
+
+        private void MIFSave_Click(object sender, RoutedEventArgs e)
+        {
+            //greatBookOfGrudges.Save();
+            //sqliteHelper.Save(greatBookOfGrudges.Records);
+            SQLiteHelper.SaveObservableCollection(greatBookOfGrudges.Records);
+            MessageBox.Show("Сохранение завершено");
+        }
+
+    }
+}
